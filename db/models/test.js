923a0f2137bf28e1da61c6ffba8c9ced883ce81e
shermanful <69591177+shermanful@users.noreply.github.com>
2021-10-02 12:09:14 -0400

    cycle 2

--- db/models/test.js
+++ db/models/test.js
@@ -1,7 +1,18 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
-const Student = require('./student');
+const Sequelize = require("sequelize");
+const db = require("../db");
+const Student = require("./student");
 
-const Test = db.define();
+const Test = db.define("test", {
+  subject: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  grade: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+});
 
+Test.belongsTo(Student); //part of model definition even though it occurs after, assigns a foreign key
+Student.hasMany(Test);
 module.exports = Test;
--- routes/students.js
+++ routes/students.js
@@ -1,4 +1,33 @@
 const router = require('express').Router();
 
+// router.get("/students", async (req,res,next) => {
+//     try {
+//         const students = await Students.findAll();
+//         res.send(userList(students));
+//       } catch (error) {
+//         next(error);
+//       }
+//     });
+// })
+// GET /users/:userId
+// router.get("/:userId", async (req, res, next) => {
+//     try {
+//       const user = await User.findByPk(req.params.userId);
+//       const pages = await Page.findAll({
+//         where: {
+//           authorId: req.params.userId
+//         }
+//       });
+  
+//       res.send(userPages(user, pages));
+//     } catch (error) {
+//       next(error);
+//     }
+//   });
+
+function () {
+    // returns an array of all people for whom tasks exist
+    return Object.keys(studentData);
+  },
 
 module.exports = router;
